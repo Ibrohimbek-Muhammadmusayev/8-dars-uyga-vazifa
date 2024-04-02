29dcea385433507d7c3b00f39197fa0cc6901443
Ibrohimbek-Muhammadmusayev <146696755+Ibrohimbek-Muhammadmusayev@users.noreply.github.com>
2024-04-02 22:43:02 +0500

    yangi pagelar qoshildi

---
+++ src/app/[id]/page.jsx
@@ -0,0 +1,25 @@
+import '@/styles/home.css'
+
+async function getDatelis(id){
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`)
+    return await response.json();
+}
+
+export default async function Page ({params}){
+    const data = await getDatelis(params.id)
+    return (
+    <>
+    <div className="card-wrapper">
+        <div className="card">
+            <img src={data.image} alt="images" width={300} height={200} className="card-images" />
+            <div className="container">
+            <h1 className="title">Title:{data.title} </h1> 
+            <p className="price">Price:{data.price}</p>
+            <p>{data.description}</p>
+            <p className="category"> Category:{data.category}</p>
+            </div>
+        </div>
+    </div>
+    </>
+    )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,64 @@
+'use client'
+
 import '@/styles/home.css'
 import axios from 'axios'
+import { Link } from 'lucide-react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-    axios.get('https://fakestoreapi.com/docs')
-    .then(res => console.log(res))
+    const [data, setData] = useState([])
+
+    const getData = async () => {
+        await axios.get('https://fakestoreapi.com/products')
+        .then((res)=> {
+            console.log(res.data);
+            setData(res.data)
+        })
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
         <div>
             <header>
-                <div className="container">
-                    <nav className='navbar'>
-                        <ul className='top-bar'>
-                            <li><a href="">Home</a></li>
-                            <li><a href="">About</a></li>
-                            <li><a href="">Contact</a></li>
-                        </ul>
-                        <img src="../images/svg/image.svg" alt="images" />
-                        <ul className='top-bar'>
-                            <li><a href="">Pages</a></li>
-                            <li><a href="">Free Version</a></li>
-                            <li><a href="">Purchase</a></li>
-                        </ul>
-                    </nav>
-                </div>
+                <nav className='bar'>
+                    <div className="container">
+                        <div className="navbar">
+                            <ul className='top-bar'>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">About</a></li>
+                                <li><a href="">Contact</a></li>
+                            </ul>
+                            <img
+                            src="/image.svg"
+                            width={100}
+                            height={50}
+                            alt="image" />
+                            <ul className='top-bar'>
+                                <li><a href="">Pages</a></li>
+                                <li><a href="">Free Version</a></li>
+                                <li><a href="">Purchase</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             </header>
             <main>
                 <div className="container">
-
+                    <div className="top-element">
+                        {data.map((item) => (
+                            <div key={item.id} className="card">
+                                <a href={`${item.id}`}>
+                                    <img className='card-images' src={item.image} alt="images" />
+                                    <h1 className='card-name'>{item.category}</h1>
+                                    <p className='card-text'>{item.title}</p>
+                                </a>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </main>
         </div>
