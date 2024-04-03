33e7f298f9b8658f1f1159bf65ec4ee7fad5b77e
Ibrohimbek-Muhammadmusayev <146696755+Ibrohimbek-Muhammadmusayev@users.noreply.github.com>
2024-04-03 22:44:52 +0500

    yangi funsiyalar qoshildi

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
     const [data, setData] = useState([])
+    const [category, setCategory] = useState('')
 
     const getData = async () => {
         await axios.get('https://fakestoreapi.com/products')
@@ -16,17 +17,31 @@ export default function Home() {
             setData(res.data)
         })
     }
-
+    
+    const hendleCategory = (e)=> {
+        setCategory(e.target.value);
+        console.log(e);
+    }
     useEffect(() => {
         getData()
     }, [])
-
+    
+    const filteredData = category
+        ? data.filter(item => item.category.toLowerCase().includes(category.toLowerCase()))
+        : data;
     return (
         <div>
             <header>
                 <nav className='bar'>
                     <div className="container">
                         <div className="navbar">
+                            <select className='dropdoun' onChange={hendleCategory}>
+                                <option value="">hammasi</option>
+                                <option value="men's clothing">Erkaklar kiyimlari</option>
+                                <option value="electronics">Elektron qurilmalar</option>
+                                <option value="jewelery">Qimmatbaho buyumlar</option>
+                                <option value="women's clothing">Ayollar kiyimlari</option>
+                            </select>
                             <ul className='top-bar'>
                                 <li><a href="">Home</a></li>
                                 <li><a href="">About</a></li>
@@ -49,7 +64,7 @@ export default function Home() {
             <main>
                 <div className="container">
                     <div className="top-element">
-                        {data.map((item) => (
+                        {filteredData.map((item) => (
                             <div key={item.id} className="card">
                                 <a href={`${item.id}`}>
                                     <img className='card-images' src={item.image} alt="images" />
